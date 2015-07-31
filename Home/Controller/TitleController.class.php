562d4c3ee159d8e849ef5477c6a0d768fe1f10e3
skingzd <kingzd@gmail.com>
2015-07-31 21:20:53 +0800

    工作之余创建title控制器基本框架

---
+++ Home/Controller/TitleController.class.php
@@ -0,0 +1,29 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class TitleController extends Controller {
+	public function search($words,$return){
+		if(!$words) $this->error('错误参数');
+		//以人名查找
+		if(strlen($words)<5){
+			$t = M('Tilte');
+			$where['name'] = array('LIKE','%'.$words.'%');
+			$data = $t->where($where)->select();
+			if(!$data) $this->error('未找到数据');
+			$data['_count'] = count($data);
+			if($return) return $data;
+			$this->assign($data);
+		}
+		//以身份证号码查找
+		if(strlen($words)<20){
+			$t = M('Tilte');
+			$data = $t->where('pid=%s',$words)->select();
+			if(!$data) $this->error('未找到数据');
+			$data['_count'] = count($data);
+			if($return) return $data;
+			$this->assign($data);
+		}
+		$this->error('未找到数据');
+	}
+}
+?>
