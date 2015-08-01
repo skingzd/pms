4c00474b19d7ecbe4499faa0b3c332d9ef81c166
kingzd <kingzd@gmail.com>
2015-08-01 21:04:29 +0800

    初步完成Department控制器编写

--- Home/Controller/DepartmentController.class.php
+++ Home/Controller/DepartmentController.class.php
@@ -2,6 +2,18 @@
 namespace Home\Controller;
 use Think\Controller;
 class DepartmentController extends Controller{
-	
+	public function search($words, $type = 'name' ,$return ){
+		if(!$words || strlen($words)>7) $this->error('参数错误');
+		$d = M('Department');
+		if($type == 'name')  $where['dm_words'] = array('LIKE','%'.$words.'%');
+		if($type == 'system') $where['dm_system'] = array('LIKE','%'.$words.'%');
+		if($type == 'id') $where['dm_id'] = $words;
+		
+		$data = $d->where($where)->select();
+		if( !$data ) $this->error('未找到数据');
+		$data['_count'] = count($data);
+		$this->assign($data);
+		if($return) return $data;
+	}
 }
 ?>
